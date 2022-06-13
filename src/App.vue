c377d4ef89e42d1c80a7fb7d558964b7b6a42fed
Elijah Miernicki <miernickielijah@gmail.com>
2022-06-13 12:54:54 -0500

    Changed component name, removed starter code from splashpage.
    renamed component and changed text.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Vue.js + TypeScript App"/>
+  <SplashPage msg="Fun Project in VUE and TypeScript"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import SplashPage from './components/SplashPage.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    SplashPage
   }
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Charis SIL', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+</style>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Charis+SIL&display=swap');
 </style>
