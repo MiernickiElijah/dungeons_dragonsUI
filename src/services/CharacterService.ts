7747cca764810ef7134ec87def1c2a2d45cafb9d
Elijah Miernicki <miernickielijah@gmail.com>
2022-06-15 11:45:11 -0500

    moved db.json file to flibbish main folder

--- src/services/CharacterService.ts
+++ src/services/CharacterService.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create ({
     //HOW DO I CONNECT WITH SQL SERVER?
-    baseURL: '#',
+    baseURL: 'https://my-json-server.typicode.com/miernickielijah/flibbish',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
